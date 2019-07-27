39419e1c12462ecb816d0effc19d404c2c723b0d
Ikenna Victor <ikennavictorofficial@gmail.com>
2019-07-27 10:34:31 +0100

    first commit

---
+++ models/genre.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+    {
+        name: { type: String, min:3, max:100, required: true }
+    }
+);
+
+// Virtual for genre's url
+GenreSchema
+.virtual('url')
+.get(function() {
+    return '/catalog/genre' + this._id;
+});
+
+// Export model
+module.exports = mongoose.model('Genre', GenreSchema);
---
+++ routes/cool.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('You\'re so cool');
+});
+
+
+module.exports = router;
